7e4f163a20a073403cd92245f168879fc865d658
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-01-07 12:39:31 -0500

    ..

--- resources/views/livewire/comments/create.blade.php
+++ resources/views/livewire/comments/create.blade.php
@@ -6,21 +6,21 @@
                 <label for="name" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{__("Name")}}</label>
                 <input type="text" id="name" wire:model="author" class="mt-1">
                 @error('author')
-                    <span class="text-sm text-red-600">{{ $message }}</span>
+                    <span class="text-sm text-red-600">{{ __($message) }}</span>
                 @enderror
             </div>
             <div class="col-span-2 sm:col-span-1">
                 <label for="email" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{__("Email")}}</label>
                 <input type="email" id="email" wire:model="email" class="mt-1">
                 @error('email')
-                    <span class="text-sm text-red-600">{{ $message }}</span>
+                    <span class="text-sm text-red-600">{{ __($message) }}</span>
                 @enderror
             </div>
             <div class="col-span-2">
                 <label for="comment" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{__("Comment")}}</label>
                 <textarea id="comment" wire:model="content" cols="30" rows="5" class="mt-1"></textarea>
                 @error('content')
-                    <span class="text-sm text-red-600">{{ $message }}</span>
+                    <span class="text-sm text-red-600">{{ __($message) }}</span>
                 @enderror
             </div>
             <div class="col-span-2">
